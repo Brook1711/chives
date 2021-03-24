9b0037b8876f15e7c3bfc82dbaed8ff119279eaa
Richard Li <33805041+Richardhongyu@users.noreply.github.com>
2021-03-24 21:05:29 +0800

    [debug] update web_v1

--- web_v1/controllers/default.go
+++ web_v1/controllers/default.go
@@ -6,26 +6,27 @@ import (
 	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
 )
 
 type DanmuRequest struct {
-	Token 	string
-	Id  string
-	Author 	int64
-	Time 	float32
-	Text 	string
-	Color 	int32
-	Type int8
+	Token  string
+	Id     string
+	Author int64
+	Time   float32
+	Text   string
+	Color  int32
+	Type   int8
 }
 
 type ReturnData struct {
 	Code int
-	Data DanmuRequest
+	Data []DanmuRequest
 }
-
 
 type MainController struct {
 	beego.Controller
@@ -47,7 +48,9 @@ func (c *DanmuController) Post() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://0.0.0.0:27017"))
-	if err != nil { return  }
+	if err != nil {
+		return
+	}
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
@@ -58,8 +61,8 @@ func (c *DanmuController) Post() {
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	//str := `{"token":"token-example","id":"vid-example","author":1000,"time":0,"text":"test","color":16777215,"type":0}`
-	danmu:=DanmuRequest{data.Token,data.Id,data.Author, data.Time, data.Text, data.Color, data.Type}
-	insertOne,err :=collection.InsertOne(ctx,danmu)
+	danmu := DanmuRequest{data.Token, data.Id, data.Author, data.Time, data.Text, data.Color, data.Type}
+	insertOne, err := collection.InsertOne(ctx, danmu)
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
@@ -68,29 +71,60 @@ func (c *DanmuController) Post() {
 }
 
 func (c *DanmuController) Get() {
-	//c.TplName = "index.tpl"
+	//var (
+	//	collection *mongo.Collection
+	//	err        error
+	//	cursor
+	//)
+
+	c.TplName = "index.tpl"
 	jsoninfo := c.GetString("id")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://0.0.0.0:27017"))
-	if err != nil { return  }
+	if err != nil {
+		return
+	}
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
 	}()
-	//err = client.Ping(ctx, readpref.Primary())
+	err = client.Ping(ctx, readpref.Primary())
 	collection := client.Database("beego_test_2").Collection("testing_2")
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+
 	defer cancel()
-	filter := bson.D{{"id",jsoninfo}}
-	var result DanmuRequest
-	err = collection.FindOne(ctx, filter).Decode(&result)
+
+	filter := bson.D{{"id", jsoninfo}}
+	cursor, err := collection.Find(context.TODO(), filter, options.Find().SetSkip(0),
+		options.Find().SetLimit(2))
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(result)
+	//延迟关闭游标
+	defer func() {
+		if err = cursor.Close(context.TODO()); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	var results []bson.D
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		fmt.Println(err)
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
+	//var result DanmuRequest
+	//err = collection.FindOne(ctx, filter).Decode(&result)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(result)
 	//filter := bson.D{{"id",jsoninfo}}
 	//var result ReturnData
 	//cur, err := collection.Find(ctx, filter)
@@ -106,10 +140,8 @@ func (c *DanmuController) Get() {
 	//	fmt.Println(err)
 	//}
 
-
 	//err = collection.Find(context.TODO(), filter)
 	//.Decode(&result)
-
 
 	if jsoninfo == "" {
 		c.Ctx.WriteString("jsoninfo is empty")
@@ -122,4 +154,3 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "brook1711@bupt.edu.cn"
 	c.TplName = "index.html"
 }
-
